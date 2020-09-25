f57e1bd88085f839cbcb68e548ed5e0486b2beb3
luck21honey <lukeadamdev@gmail.com>
2020-09-25 07:14:44 +0300

    add axios request

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
 import express from "express"
+import axios from 'axios'
 
 const app = express()
 
+const API_URL = "https://nodeapi.lamsal.repl.co/"
+
 app.get('/', (req: any, res: any) => {
-    res.json('API is working')
+    axios.get(API_URL)
+        .then((response: any) => {
+            let result = JSON.stringify(response.data)
+            console.log('result>>', result)
+            res.json('API is working')
+
+        })
+        .catch(err => {
+            console.log('error>>', err)
+        })
 })
 
 app.listen(8000, () => {
     console.log("server started at http://localhost:8000")
 })
+
